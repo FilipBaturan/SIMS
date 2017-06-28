7721d269b41e92460b9baeffcf685c1241c39ad6
FilipBaturan <filip.baturan@gmail.com>
2017-06-28 16:51:42 +0200

    GlavniOkvir

--- Aplikacija/src/Model/Main.java
+++ Aplikacija/src/Model/Main.java
@@ -1,9 +1,15 @@
 package Model;
 
+import javax.swing.JFrame;
+
+import GUI.GlavniOkvir;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Jeeeeeeeeeeeeeee\n");
+		GlavniOkvir go = new GlavniOkvir();
+		
 
 	}
 
